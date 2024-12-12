add30324d3e2c11c60b5f57ad09274348757a045
Razvan Pricop <razvanipricop@gmail.com>
2024-12-12 22:36:40 +0100

    Initial commit

---
+++ Bookworm/Bookworm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Razvan Pricop on 12.12.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
